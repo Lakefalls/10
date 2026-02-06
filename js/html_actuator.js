99a0b45d4380a88d37d37175c4f22bba2ac3f4be
Lakefalls <124891316+Lakefalls@users.noreply.github.com>
2026-02-06 20:54:06 +0000

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -87,6 +87,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   if (tile.value === -18) inner.textContent = 19;
   if (tile.value === -20) inner.textContent = 20;
   if (tile.value === -21) inner.textContent = 21;
+  if (tile.value === -23) inner.textContent = 23;
   if (tile.value === -25) inner.textContent = 25;
   if (tile.value === -27) inner.textContent = 27;
   if (tile.value === -28) inner.textContent = 28;
